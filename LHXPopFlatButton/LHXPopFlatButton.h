96f44f29647426b3365cd5b45a3942c068008304
Zitao Xiong <caoer115@gmail.com>
2017-07-14 18:54:12 -0400

    add framework refs

---
+++ LHXPopFlatButton/LHXPopFlatButton.h
@@ -0,0 +1,21 @@
+//
+//  LHXPopFlatButton.h
+//  LHXPopFlatButton
+//
+//  Created by Zitao Xiong on 7/14/17.
+//  Copyright © 2017 Zitao Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LHXPopFlatButton.
+FOUNDATION_EXPORT double LHXPopFlatButtonVersionNumber;
+
+//! Project version string for LHXPopFlatButton.
+FOUNDATION_EXPORT const unsigned char LHXPopFlatButtonVersionString[];
+
+
+#import <LHXPopFlatButton/VBFDoubleSegment.h>
+#import <LHXPopFlatButton/VBFPopFlatButton.h>
+#import <LHXPopFlatButton/UIColor+FlatColors.h>
+
